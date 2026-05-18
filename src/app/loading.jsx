638a8dd83cc300a822c9dd5f27458b621aa1af9a
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-18 23:58:34 +0600

    loading and not-found page is added

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function LoadingPage() {
+  return (
+    <div>
+        <h1>This is Loading Page</h1>
+        <h2>Loading...</h2>
+    </div>
+  )
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFountPage() {
+  return (
+    <div>
+        <h1>404</h1>
+        <p>something is rong this page is not found!</p>
+    </div>
+  )
+}
